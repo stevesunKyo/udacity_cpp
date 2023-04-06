b97b2b6cd6293d70eaf023317278ed54043f98b1
Sun208 <tl.sun.123o@gmail.com>
2023-04-06 17:18:09 +0900

    1

---
+++ HelloWorld/src/matrix.cpp
@@ -0,0 +1,27 @@
+#include "matrix.h"
+
+
+/* 
+  public:
+  mat(vector<vector<float>> data);  // constructor
+  ~mat(); // destructor
+  mat(const mat &source); // copy constructor
+  mat(mat &&source); // move constructor
+  mat& operator=(mat &&source); // copy assignment operator
+  
+  mat dot(mat B); // calculate dot product of A*B, when B is a matrix
+  vec dot(vec B); // calculate dot product of A*B, when B is a vector
+    
+    
+  mat operator+ (mat m1, mat m2); // element-wise plus
+  mat operator- (mat m1, mat m2); // element-wise substract
+  mat operator* (mat m1, mat m2); // element-wise multiply
+  mat operator/ (mat m1, mat m2); // element-wise divide
+    
+    
+  private:
+  int _col; // row number
+  int _row; // column number 
+  vector<vector<float>> _data; // matrix data*/
+mat::mat(vector<vector >)
+
